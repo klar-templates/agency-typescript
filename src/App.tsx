cc4bc800a321ec78f02b84a755b7eecb3b296ea3
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-18 14:14:23 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Page from './components/Page';
 import Page1 from './components/Page1';
 import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(undefined);
+  
+  useEffect(() => {
+    function init(siteName: string) {
+      if (!localStorage.getItem('data')) {
+        async function data() {
+          const response = await fetch(
+            `https://raw.githubusercontent.com/klar-sites/${siteName}/master/data.json`);
+          return response.json();
+        }
+
+        data()
+          .then((data) => {
+            localStorage.setItem('data', JSON.stringify(data));
+            setData(data);
+          });
+        } else {
+          const data = JSON.parse(localStorage.getItem('data') as string);
+          setData(data);
+        }
+    }
+    
+    // This is for one page website
+    // Param: Site name in Klar
+    init('agency');
+    // return () => clearInterval(id);
+  }, []);
+  
+  if (!data) {
+    return null;
+  }
+
+  console.log(data);
 
   return (
     <div className="App">
@@ -21,28 +49,6 @@ function App() {
           <Route path="/sida-1" element={<Page1 />} />
         </Routes>
       </Router>
-      <div className="nice-to-have remove-soon">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
     </div>
   )
 }
