7641730fe58898099c4de57d3d6e4fbbd3bd7146
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-21 01:21:32 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
     if (window.klarContext.isInKlar) {
       // console.log(typeof window.klar !== 'undefined')
       parent.frames.window.klar['setData'] = setData;
-      setData(parent.frames.window.klar.data);
+      setTimeout(() => setData(parent.frames.window.klar.data), 1000);
       // parent.frames.window.reactPageIsLoaded();
       // const currentPage = parent.frames.window.klar.sdk.currentPage.get();
       // window.location.href = currentPage._path;
