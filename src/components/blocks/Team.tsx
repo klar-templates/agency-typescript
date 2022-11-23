dcdac44c1481849a42bcc50e7754f152504e9d4f
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-23 01:32:42 +0100

    wip

--- src/components/blocks/Team.tsx
+++ src/components/blocks/Team.tsx
@@ -26,8 +26,8 @@ export default function Team(data: any) {
     return (
       <div className="row">
         <div className="col-lg-12 text-center">
-          <h2 className="section-heading">{heading}</h2>
-          <h3 className="section-subheading text-muted">{subHeading}</h3>
+          <h2 className="section-heading" data-field-string="heading">{heading}</h2>
+          <h3 className="section-subheading text-muted" data-field-string="sub_heading">{subHeading}</h3>
         </div>
       </div>
     );
@@ -44,6 +44,8 @@ export default function Team(data: any) {
             image={member.image}
             name={member.heading}
             position={member.text}
+            socialLinks={member.social_links}
+            index={i}
             key={i}
         />
         })
@@ -51,24 +53,25 @@ export default function Team(data: any) {
       </div>
     );
   }
-  function TeamMember({name, position, image, socialLinks}: any) {
+  function TeamMember({name, position, image, socialLinks, index}: any) {
     return (
       <div className="col-sm-4">
-        <div className="team-member" data-field-array-item="">
-          <img src={image} className="img-responsive img-circle" no-data-placement="top" no-data-inset="true" alt={name} />
-          <h4>{name}</h4>
-          <p>{position}</p>
+        <div className="team-member" data-field-array-item>
+          <img src={image} className="img-responsive img-circle" no-data-placement="top" no-data-inset="true" data-field-string-file={`team[${index}].image`} alt={name} />
+          <h4 data-field-string={`team[${index}].heading`}>{name}</h4>
+          <p data-field-string={`team[${index}].text`} data-toolbar-buttons="['bold', 'italic', 'underline', 'anchor']">{position}</p>
           <ul className="list-inline social-buttons">
-            <li>
-              <a href="#youraccount"><i className="fa fa-twitter"></i></a>
+          { socialLinks.map((socialLink: any, i: any) => 
+            <li key={i}>
+              <a
+                href={socialLink.link}
+                data-field-string-ui-widget-icon={`team[${index}].social_links[${i}].icon`}
+                data-field-string-format-uri={`team[${index}].social_links[${i}].link`}
+                data-placement="bottom">
+                <i className={`fa fa-${socialLink.icon}`}></i>
+              </a>
             </li>
-            <li>
-              <a href="#youraccount"><i className="fa fa-facebook"></i></a>
-            </li>
-  
-            <li>
-              <a href="#youraccount"><i className="fa fa-instagram"></i></a>
-            </li>
+          )}
           </ul>
         </div>
       </div>
@@ -78,7 +81,7 @@ export default function Team(data: any) {
     return (
       <div className="row">
         <div className="col-lg-8 col-lg-offset-2 text-center">
-          <p className="large">{text}</p>
+          <p className="large" data-field-text="text" data-toolbar-buttons="['bold', 'italic', 'underline', 'anchor']">{text}</p>
         </div>
       </div>
     );
