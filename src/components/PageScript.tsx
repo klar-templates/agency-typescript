35856bbfdba1f8fa6c3728216a7a9d1d1c0d1e57
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-25 15:44:26 +0100

    wip

--- src/components/PageScript.tsx
+++ src/components/PageScript.tsx
@@ -4,61 +4,7 @@ export default function PageScript() {
   );
  
   function Script() {
-    const pageScript = `
-      <!-- Theme JavaScript -->
-      <script>
-        // Agency Theme JavaScript
-        (function($) {
-          "use strict"; // Start of use strict
-          
-          // jQuery for page scrolling feature - requires jQuery Easing plugin
-          $('a.page-scroll').bind('click', function(event) {
-              var $anchor = $(this);
-              if (!$anchor.attr('href')) return;
-              if (!$anchor.attr('href').startsWith('#')) return;
-              if (!$($anchor.attr('href')).length) return;
-              $('html, body').stop().animate({
-                scrollTop: ($($anchor.attr('href')).offset().top - 50)
-                // scrollTop: ($($anchor.attr('href')).offset().top)
-              }, 1250, 'easeInOutExpo');
-              event.preventDefault();
-          });
-          
-          // Highlight the top nav as scrolling occurs
-          $('body').scrollspy({
-              target: '.navbar-fixed-top',
-              offset: 51
-          });
-          
-          // Closes the Responsive Menu on Menu Item Click
-          $('.navbar-collapse ul li a').click(function(){ 
-            $('.navbar-toggle:visible').click();
-          });
-          
-          // Offset for Main Navigation
-          $('#main-nav').affix({
-            offset: {
-              top: 100
-            }
-          });
-        })(jQuery);
-      </script>
-
-      <!-- Klarscript, executes when page is loaded. -->
-      <script>
-        function klarScript() {
-          // Add 'klar-modal-open' CSS class, when modal is opened
-          $('.modal').on('show.bs.modal', function (event) {
-            var modal = this;
-            modal.classList.add('klar-modal-open');
-          });
-          $('.modal').on('hide.bs.modal', function (event) {
-            var modal = this;
-            modal.classList.remove('klar-modal-open');
-          });
-        }
-        klarScript();
-      </script>`;
+    const pageScript = ``;
     return <div dangerouslySetInnerHTML={{__html: pageScript}} />;
   }
 }
