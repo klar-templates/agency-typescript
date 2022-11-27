61ee4f9ecd8aa6c40f75fa6ca0232304c2373388
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-27 14:22:39 +0100

    wip

--- src/components/blocks/Hero.tsx
+++ src/components/blocks/Hero.tsx
@@ -1,13 +1,13 @@
 export default function Hero(data: any) {
   const {_id, _type, show_button, show_subtitle, show_title, title, subtitle, link, link_text, style, image } = data.block;
   return (
-    <div className="container px-6 py-16 mx-auto">
+    <div className="g-surface-variant ontainer px-6 py-16 mx-auto">
         <div className="items-center lg:flex">
             <div className="w-full lg:w-1/2">
                 <div className="lg:max-w-lg">
-                    <h1 className="text-2xl font-semibold text-gray-800 uppercase dark:text-white lg:text-3xl">Best Place To Choose Your Clothes</h1>
-                    <p className="mt-2 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
-                    <button className="w-full tracking-wider px-6 py-2.5 mt-6 text-sm text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-md lg:w-auto hover:bg-blue-500 focus:outline-none focus:bg-blue-500">Shop Now</button>
+                    <h1 className="text-2xl font-semibold text-on-surface uppercase lg:text-3xl">Best Place To Choose Your Clothes</h1>
+                    <p className="mt-2 text-on-surface-variant dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro beatae error laborum ab amet sunt recusandae? Reiciendis natus perspiciatis optio.</p>
+                    <button className="bg-primary text-on-primary w-full tracking-wider px-6 py-2.5 mt-6 text-sm uppercase transition-colors duration-300 transform rounded-md lg:w-auto hover:drop-shadow-lg focus:outline-none focus:drop-shadow-sm">Shop Now</button>
                 </div>
             </div>
 
