26d86a97ae8c7c72799c9b00d1a45bc287a53c62
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-28 03:42:53 +0100

    wip

--- src/components/blocks/Header2.tsx
+++ src/components/blocks/Header2.tsx
@@ -19,8 +19,8 @@ export default function Header2(data: any) {
           <nav className="flex h-9 items-center justify-between" aria-label="Global">
             <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+                <span className="r-only h-8 text-2xl text-sky-600 ppercase">Your logo</span>
+                {/* <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
               </a>
             </div>
             <div className="flex lg:hidden">
