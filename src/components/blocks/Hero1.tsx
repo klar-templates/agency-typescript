8a4a04715ec2071b4cfdf7f8cd8a638c62692c2d
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-28 02:10:19 +0100

    wip

--- src/components/blocks/Hero1.tsx
+++ src/components/blocks/Hero1.tsx
@@ -1,34 +1,34 @@
 export default function Hero1(data: any) {
   const {_id, _type, show_button, show_subtitle, show_title, title, subtitle, link, link_text, style, image } = data.block;
   return (
-    <div className="relative isolate bg-white border-t">
+    <div className="text-on-background relative isolate bg-white border-t h-scree g-[url('https://raw.githubusercontent.com/klar-templates/agency/master/img/header-bg.jpg')]">
       <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
         <svg className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)" fillOpacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
           <defs>
             <linearGradient id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533" x1="1155.49" x2="-78.208" y1=".177" y2="474.645">
-              <stop stopColor="#9089FC"></stop>
-              <stop offset="1" stopColor="#FF80B5"></stop>
+              <stop stopColor="#3b82f6"></stop>
+              <stop offset="1" stopColor="#3b82f6"></stop>
             </linearGradient>
           </defs>
         </svg>
       </div>
-      <main>
+      <main className="relative z-10">
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-3xl px-16 pb-32 sm:pt-48 sm:pb-40">
             <div>
-              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                 <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
                   <span className="text-gray-600">
                     Announcing our next round of funding. <a href="#" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
                   </span>
                 </div>
-              </div>
+              </div> */}
               <div>
                 <h1 className="text-4xl font-bold tracking-tight sm:text-center sm:text-6xl">Data to enrich your online business</h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+                <p className="text-on-surface mt-6 text-lg leading-8 sm:text-center">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
                 <div className="mt-8 flex gap-x-4 sm:justify-center">
-                  <a href="#" className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
+                  <a href="#" className="text-inverse-on-surface inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
                     Get started
                     <span className="text-indigo-200" aria-hidden="true">&rarr;</span>
                   </a>
@@ -38,7 +38,7 @@ export default function Hero1(data: any) {
                   </a>
                 </div>
               </div>
-              <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
+              {/* <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
                 <svg className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]" viewBox="0 0 1155 678" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)" fillOpacity=".3" d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z" />
                   <defs>
@@ -54,11 +54,12 @@ export default function Hero1(data: any) {
                     </linearGradient>
                   </defs>
                 </svg>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
       </main>
+      {/* <div className="absolute inset-0 bg-black opacity-[0.4]"></div> */}
     </div>
   );
 }
