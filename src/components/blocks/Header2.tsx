69873dddcf53fcd902ef88effe559b3fd626f016
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-28 16:49:30 +0100

    wip

--- src/components/blocks/Header2.tsx
+++ src/components/blocks/Header2.tsx
@@ -19,7 +19,7 @@ export default function Header2(data: any) {
           <nav className="flex h-9 items-center justify-between" aria-label="Global">
             <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="r-only h-8 text-2xl text-sky-600 ppercase">Your logo</span>
+                <span className="r-only h-8 text-2xl text-primary ppercase">Your logo</span>
                 {/* <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
                 {/* <img className="h-8" src="https://demo.kaliumtheme.com/main/wp-content/uploads/2015/03/99c8d4a6236909e8ca6595f17d3beb42.png" alt="" /> */}
                 
--- src/components/blocks/Hero2.tsx
+++ src/components/blocks/Hero2.tsx
@@ -6,29 +6,34 @@ export default function Hero2(data: any) {
         <div className="mx-auto grid max-w-[40rem] grid-cols-1 gap-y-16 gap-x-8 lg:max-w-none lg:grid-cols-3">
           <div className="flex flex-col lg:pb-6">
             <h1 className="mt-4 text-4xl font-extrabold leading-none tracking-tight text-slate-900 sm:text-5xl sm:leading-[3.5rem]">Transmit</h1>
-            <p className="order-first text-base font-semibold leading-7 text-sky-500">Podcast template</p>
-            <p className="mt-6 text-base leading-7 text-slate-700">You’ve ordered your SM7B, boom arm, and a bunch of acoustic foam. With this template, you can finally stop procrastinating and record your first episode.</p>
+            <p className="order-first text-base font-semibold leading-7 text-primary">Podcast template</p>
+            <p className="mt-6 text-base leading-7 text-on-surface">You’ve ordered your SM7B, boom arm, and a bunch of acoustic foam. With this template, you can finally stop procrastinating and record your first episode.</p>
             <div className="flex flex-wrap mt-6">
-              <div className="flex items-center text-sm font-medium text-slate-500">
+              <div className="flex items-center text-sm font-medium text-on-surface-variant">
                 <svg fill="none" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 flex-none stroke-current text-slate-400" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
                   <ellipse cx="16" cy="16" rx="13" ry="5"></ellipse>
                   <ellipse cx="16" cy="16" rx="13" ry="5" transform="rotate(60 16 16)"></ellipse>
                   <ellipse rx="13" ry="5" transform="matrix(-.5 .86603 .86603 .5 16 16)"></ellipse>
-                  <circle cx="16" cy="16" r="2"></circle></svg><span className="ml-2.5">React</span>
-                  </div>
-                  <div className="flex items-center text-sm font-medium text-slate-500 ml-6">
-          <svg fill="none" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 flex-none stroke-current text-slate-400" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-            <defs>
-              <linearGradient id="next-icon-gradient-a" x1="15.125" y1="18.25" x2="24.25" y2="27.375" gradientUnits="userSpaceOnUse"><stop offset=".24"
-              stopColor="currentColor"></stop><stop offset="1" stopColor="currentColor" stopOpacity="0"></stop>
-              </linearGradient>
-              <linearGradient id="next-icon-gradient-b" x1="20.5" y1="11.25" x2="20.5" y2="18.25" gradientUnits="userSpaceOnUse">
-                <stop stopColor="currentColor"></stop><stop offset="1" stopColor="currentColor" stopOpacity="0"></stop>
-                </linearGradient></defs><path d="M16.25 28.5c6.765 0 12.25-5.485 12.25-12.25S23.015 4 16.25 4 4 9.485 4 16.25 9.485 28.5 16.25 28.5Z"></path>
-                <path d="M12 21.5V12l12.25 14.25" stroke="url(#next-icon-gradient-a)"></path>
-                <path d="M21.25 12a.75.75 0 1 0-1.5 0v8.17l1.5 1.64V12Z" fill="url(#next-icon-gradient-b)" strokeWidth="0"></path>
-                </svg><span className="ml-2.5">Next.js</span>
-                </div></div><div className="mt-10 flex gap-4">
+                  <circle cx="16" cy="16" r="2"></circle>
+                </svg>
+                <span className="ml-2.5">React</span>
+              </div>
+            <div className="flex items-center text-sm font-medium text-slate-500 ml-6">
+              <svg fill="none" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 flex-none stroke-current text-slate-400" strokeWidth="1.5" strokeLinecap="round"
+              strokeLinejoin="round">
+                <defs>
+                  <linearGradient id="next-icon-gradient-a" x1="15.125" y1="18.25" x2="24.25" y2="27.375" gradientUnits="userSpaceOnUse"><stop offset=".24"
+                  stopColor="currentColor"></stop><stop offset="1" stopColor="currentColor" stopOpacity="0"></stop>
+                  </linearGradient>
+                  <linearGradient id="next-icon-gradient-b" x1="20.5" y1="11.25" x2="20.5" y2="18.25" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="currentColor"></stop><stop offset="1" stopColor="currentColor" stopOpacity="0"></stop>
+                    </linearGradient></defs><path d="M16.25 28.5c6.765 0 12.25-5.485 12.25-12.25S23.015 4 16.25 4 4 9.485 4 16.25 9.485 28.5 16.25 28.5Z"></path>
+                    <path d="M12 21.5V12l12.25 14.25" stroke="url(#next-icon-gradient-a)"></path>
+                    <path d="M21.25 12a.75.75 0 1 0-1.5 0v8.17l1.5 1.64V12Z" fill="url(#next-icon-gradient-b)" strokeWidth="0"></path>
+              </svg>
+              <span className="ml-2.5">Next.js</span>
+            </div>
+                </div><div className="mt-10 flex gap-4">
                   <a href="https://transmit.tailwindui.com" className="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-slate-900 text-white hover:bg-slate-700" target="_blank">
                     <span><span className="inline-flex items-center"><span>Live preview</span>
                     <svg viewBox="0 0 20 20" className="ml-1.5 h-5 w-5 fill-slate-400" fillRule="evenodd" clipRule="evenodd">
