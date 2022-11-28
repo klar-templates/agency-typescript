766b6dc2bbddad3d8e68e3eefa56eb34c0f53a06
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-29 00:54:50 +0100

    wip

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -1,85 +1,85 @@
-import Layout from './Layout'
-import Nunjucks from './blocks/bootstrap/Nunjucks'
-import KlarEditBlock from './KlarEditBlock'
-import conf from '../../config.json'
-import { useEffect, useState } from 'react'
+import Layout from './Layout';
+import Nunjucks from './blocks/bootstrap/Nunjucks';
+import KlarEditBlock from './KlarEditBlock';
+import conf from '../../config.json';
+import { useEffect, useState } from 'react';
 
 export default function Components(data: any) {
   const [config, setConfig] = useState(
     window.klarContext.isInKlar
       ? parent.frames.window.klar.config.data_default
       : undefined,
-  )
+  );
   useEffect(() => {
     if (window.klarContext.isInKlar) {
       // parent.frames.window.reactPageIsLoaded();
     } else {
       // Param: Site name in Klar
       // console.log('whhhhhhaaaat')
-      setConfig(conf)
+      setConfig(conf);
       // getData('agency-typescript');
     }
     setTimeout(() => {
-      const blocks = [document.querySelectorAll('.js-block')]
-      let blocksHeight = 0
+      const blocks = [document.querySelectorAll('.js-block')];
+      let blocksHeight = 0;
       blocks.forEach((b: any) => {
-        blocksHeight += b.offsetHeight
-      })
-      blocksHeight = blocksHeight * 0.8
-      const bodyHeight = document.body.offsetHeight
-      const newHeight = bodyHeight - blocksHeight
-      document.body.style.paddingBottom = '15px'
-      document.documentElement.style.maxHeight = '0px'
-    }, 100)
+        blocksHeight += b.offsetHeight;
+      });
+      blocksHeight = blocksHeight * 0.8;
+      const bodyHeight = document.body.offsetHeight;
+      const newHeight = bodyHeight - blocksHeight;
+      document.body.style.paddingBottom = '15px';
+      document.documentElement.style.maxHeight = '0px';
+    }, 100);
     // return () => clearInterval(id);
-  }, [])
+  }, []);
 
   function getData(template: string) {
-    const cacheKey = 'klar-config'
-    let parsedData
+    const cacheKey = 'klar-config';
+    let parsedData;
     if (!localStorage.getItem(cacheKey)) {
       async function requestData() {
         const response = await fetch(
           `https://raw.githubusercontent.com/klar-templates/${template}/master/config.json`,
-        )
-        return response.json()
+        );
+        return response.json();
       }
       requestData().then((config) => {
         // console.log(config);
         // localStorage.setItem(cacheKey, JSON.stringify(data));
-        setConfig(config)
-      })
+        setConfig(config);
+      });
     } else {
-      const config = JSON.parse(localStorage.getItem(cacheKey) as string)
-      setConfig(config)
+      const config = JSON.parse(localStorage.getItem(cacheKey) as string);
+      setConfig(config);
     }
   }
 
   if (!config) {
-    return null
+    return null;
   }
   // console.log('tiiiiiiiime')
 
-  const dataDefaults = config.data_defaults.blocks
-  const Components = data.components
+  const dataDefaults = config.data_defaults.blocks;
+  const Components = data.components;
   // console.log('heloo', Components)
   // console.log('helooasdfas dfasdf asdfasdf asdf')
-  const blockTypes = config.block_types
+  const blockTypes = config.block_types;
   // console.log('blockTypes', blockTypes)
-  const blockArray: any = []
+  const blockArray: any = [];
   blockTypes.forEach((blockType: any) => {
     // console.log(blockType)
-    const block: any = dataDefaults[blockType.name]
+    const block: any = dataDefaults[blockType.name];
     // This is going go be fixed. Only block needs to be forwarded in the future.
     const blockData: any = {
       block: { ...block, _id: blockType.name, _type: blockType.name },
-    }
+    };
 
-    let componentName = blockType.name
+    let componentName = blockType.name;
     componentName =
-      componentName.charAt(0).toUpperCase() + componentName.slice(1)
+      componentName.charAt(0).toUpperCase() + componentName.slice(1);
     // console.log('componentName', componentName)
-    const DynamicComponent = Components[componentName]
+    const DynamicComponent = Components[componentName];
     if (DynamicComponent) {
       blockArray.push(
         <KlarEditBlock
@@ -90,7 +90,7 @@ export default function Components(data: any) {
         >
           <DynamicComponent {...blockData} />
         </KlarEditBlock>,
-      )
+      );
     } else {
       blockArray.push(
         <KlarEditBlock
@@ -100,16 +100,16 @@ export default function Components(data: any) {
         >
           <Nunjucks {...blockData} />
         </KlarEditBlock>,
-      )
+      );
     }
-  })
+  });
 
   return (
     <>
       <Layout {...data}>{blockArray}</Layout>
       <Style />
     </>
-  )
+  );
 }
 
 function Style() {
@@ -148,6 +148,6 @@ function Style() {
       order-radius: inherit;
     }
   </style>
-  `
-  return <div dangerouslySetInnerHTML={{ __html: style }} />
+  `;
+  return <div dangerouslySetInnerHTML={{ __html: style }} />;
 }
--- src/components/blocks/bootstrap/Nunjucks.tsx
+++ src/components/blocks/bootstrap/Nunjucks.tsx
@@ -3,15 +3,16 @@ import { useEffect, useState } from 'react';
 
 export default function Nunjucks(data: any) {
   const [template, setTemplate] = useState('');
-  
+
   useEffect(() => {
     // Get Nunjucks template
     if (window.klarContext.isInKlar) {
-      let template = parent.frames.window.klar.templates.blocks[data.block._type];
+      let template =
+        parent.frames.window.klar.templates.blocks[data.block._type];
       if (template) {
         setTemplate(template.content);
       } else {
-        console.log('No template content for this block:', data.block._type)
+        console.log('No template content for this block:', data.block._type);
       }
     } else {
       getTemplate(data.block._type);
@@ -21,34 +22,28 @@ export default function Nunjucks(data: any) {
 
   // This is when you're in this application, when in Klar get the template file from the Klar application.
   function getTemplate(templateName: string) {
-    const cacheKey = 'klar-nunjucks-template';
+    const cacheKey = 'klar-nunjucks-template-' + templateName;
     let data = '';
     if (!localStorage.getItem(cacheKey)) {
       async function requestData() {
-        const response = await fetch(
-          `/blocks/${templateName}.html`);
+        const response = await fetch(`/blocks/${templateName}.html`);
         return response.text();
       }
-      requestData()
-        .then((data) => {
-          // localStorage.setItem(cacheKey, data);
-          setTemplate(data);
-        });
-      } else {
-        const data = localStorage.getItem(cacheKey) as string;
+      requestData().then((data) => {
+        localStorage.setItem(cacheKey, data);
         setTemplate(data);
-      }
+      });
+    } else {
+      const data = localStorage.getItem(cacheKey) as string;
+      setTemplate(data);
+    }
   }
 
   if (!template) {
     return null;
   }
-  
+
   const renderedTemplate = window.nunjucks.renderString(template, data);
-  
-  return (
-    <div
-      dangerouslySetInnerHTML={{__html: renderedTemplate}}
-    />
-  );
+
+  return <div dangerouslySetInnerHTML={{ __html: renderedTemplate }} />;
 }
