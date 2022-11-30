b2530cb9a4b9111d04f892c83f0416d2df40e7c5
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-01 00:24:34 +0100

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ReactDOMServer from 'react-dom/server';
+import { StaticRouter } from 'react-router-dom/server';
 import App from './App';
 import './index.css';
 
 if (window.releaseReactApp) {
   ReactDOMServer.renderToString(
-    <React.StrictMode>
+    <StaticRouter location={localStorage.getItem('current-page') as string}>
       <App />
-    </React.StrictMode>,
+    </StaticRouter>,
   );
 } else {
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
