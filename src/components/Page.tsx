1a2e0e20c6ca91dd5e4f687fec87a1a8d3381b59
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-01 00:18:54 +0100

    wip

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -38,6 +38,7 @@ const Components: any = {
 };
 
 export default function Page(data: IData) {
+  console.log('varför', data);
   createContext(data);
   if (window.klarContext.data.pages.length === 0) {
     // Tell the user that there are no pages yet.
