fbec99568bda8480f21ba78706d923f4a6cb0e15
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-01 00:05:17 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -76,10 +76,10 @@ function App() {
   } else {
     startpage._path = '/';
   }
-  // console.log('currentPageInKlar', currentPageInKlar)
-  // console.log('window.klarContext.isInKlar', window.klarContext.isInKlar)
-  // console.log('location.href', location.href)
-  // console.log('location.pathname', location.pathname)
+  console.log('currentPageInKlar', currentPageInKlar);
+  console.log('window.klarContext.isInKlar', window.klarContext.isInKlar);
+  console.log('location.href', location.href);
+  console.log('location.pathname', location.pathname);
 
   return (
     <HelmetProvider>
