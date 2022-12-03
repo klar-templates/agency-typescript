99837346e0d60e06e14b91b5b5d85d5d0575d8c5
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-03 23:19:37 +0100

    wip

--- src/components/KlarEditBlock.tsx
+++ src/components/KlarEditBlock.tsx
@@ -1,10 +1,18 @@
-export default function KlarEditBlock({id, type, children, isInKlar}: any) {
+export default function KlarEditBlock({ id, type, children, isInKlar }: any) {
+  if (window.prodConfig) {
+    return children;
+  }
+
   if (!window.klarContext.isInKlar && !isInKlar) {
     return children;
   }
-  
+
   return (
-    <div data-id={id} data-template-id={type} className={`${type} js-klar-block js-block klar-block`}>
+    <div
+      data-id={id}
+      data-template-id={type}
+      className={`${type} js-klar-block js-block klar-block`}
+    >
       {children}
     </div>
   );
