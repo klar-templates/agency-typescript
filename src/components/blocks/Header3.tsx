7cdf82c0ea079d4356da3035ad49cfee0e7ec558
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-07 20:50:39 +0100

    wip

--- src/components/blocks/Header3.tsx
+++ src/components/blocks/Header3.tsx
@@ -19,14 +19,12 @@ export default function Header3(data: any) {
           aria-label="Global"
         >
           <div className="flex lg:min-w-0 lg:flex-1" aria-label="Global">
-            <Link to={window.siteConfig.publicPath} className="-m-1.5 p-1.5">
-              <span
-                style={{ pointerEvents: 'none' }}
-                className="r-only text-2xl font-bold ext-primary-dark text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300"
-                data-field-string="logo_text"
-              >
-                {logo_text}
-              </span>
+            <Link
+              to={window.siteConfig.publicPath}
+              className="r-only text-2xl font-bold ext-primary-dark text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300"
+              data-field-string="logo_text"
+            >
+              {logo_text}
               {/* <span className="r-only text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300">Your logo</span> */}
               {/* <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
               {/* <img className="h-8" src="https://demo.kaliumtheme.com/main/wp-content/uploads/2015/03/99c8d4a6236909e8ca6595f17d3beb42.png" alt="" /> */}
