ca557d0a4e189cc8e3019268f47ef954a252bb39
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-09 15:13:38 +0100

    wip

--- src/components/blocks/Header3.tsx
+++ src/components/blocks/Header3.tsx
@@ -32,7 +32,7 @@ export default function Header3(data: any) {
     } else if (style.gradient === 'bg-gradient-to-tl') {
       direction = 'top left';
     }
-    backgroundImage: `linear-gradient(to ${direction}, ${style.gradientFrom}, ${style.gradientTo}`;
+    backgroundImage = `linear-gradient(to ${direction}, ${style.gradientFrom}, ${style.gradientTo}`;
   }
   const classes: any = classNames({
     'klar-outline relative z-10 lg:px-8': true,
