2ec5fa3d6da04aa1ee660216e78eddcc4d3d7eba
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-14 17:09:57 +0100

    wip

--- src/components/blocks/Header3.tsx
+++ src/components/blocks/Header3.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from '../../utils/classNames';
 
@@ -9,6 +10,7 @@ const navigation = [
 ];
 
 export default function Header3(data: any) {
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
   const { _id, _type, logo_text, style } = data.block;
 
   let backgroundImage;
@@ -76,7 +78,7 @@ export default function Header3(data: any) {
 
   return (
     <header className={classes} style={styles}>
-      <div className="py-6 mx-auto container">
+      <div className="py-6 px-6 mx-auto lg:px-0 container">
         <nav
           className="flex h-9 items-center justify-between"
           aria-label="Global"
@@ -100,10 +102,26 @@ export default function Header3(data: any) {
           <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuVisible(true)}
+              className="inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             >
               <span className="sr-only">Open main menu</span>
               {/* <Bars3Icon className="h-6 w-6" aria-hidden="true" /> */}
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+              </svg>
             </button>
           </div>
           <div className="hidden lg:flex lg:min-w-0 lg:shrink lg:justify-end lg:gap-x-12">
@@ -142,33 +160,79 @@ export default function Header3(data: any) {
             </a>
           </div> */}
         </nav>
-        <div>
+        {mobileMenuVisible && (
           <div className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
             <div className="flex h-9 items-center justify-between">
               <div className="flex">
-                <a href="#" className="-m-1.5 p-1.5">
+                {/* <a href="#" className="-m-1.5 p-1.5">
                   <span className="sr-only">Your Company</span>
                   <img
                     className="h-8"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                     alt=""
                   />
-                </a>
+                </a> */}
+                <Link
+                  to={window.siteConfig.publicPath}
+                  className="js-klar-logo text-primary-dark dark:text-primary r-only text-2xl font-bold ext-gray-800 transition-colors duration-300 transform lg:text-3xl hover:text-gray-700 dark:hover:text-primary"
+                  style={{
+                    fontFamily:
+                      '"Kaushan Script","Helvetica Neue",Helvetica,Arial,sans-serif',
+                  }}
+                  data-field-string="logo_text"
+                >
+                  {logo_text}
+                  {/* <span className="r-only text-2xl font-bold text-gray-800 transition-colors duration-300 transform dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300">Your logo</span> */}
+                  {/* <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
+                  {/* <img className="h-8" src="https://demo.kaliumtheme.com/main/wp-content/uploads/2015/03/99c8d4a6236909e8ca6595f17d3beb42.png" alt="" /> */}
+                </Link>
               </div>
               <div className="flex">
                 <button
                   type="button"
-                  className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                  className="inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
                 >
                   <span className="sr-only">Close menu</span>
                   {/* <XMarkIcon className="h-6 w-6" aria-hidden="true" /> */}
+                  <svg
+                    className="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    onClick={() => setMobileMenuVisible(false)}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
                 </button>
               </div>
             </div>
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
                 <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
+                  {window.klarContext.data.pages.map((page: any, i: any) => (
+                    <li key={i} className={'block'}>
+                      <Link
+                        to={i === 0 ? window.siteConfig.publicPath : page._path}
+                        className={
+                          page._id ===
+                          (window.klarContext.currentPage &&
+                            window.klarContext.currentPage._id)
+                            ? '-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10 text-primary-dark dark:text-primary'
+                            : '-mx-3 block rounded-lg py-2 px-3 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-400/10'
+                        }
+                      >
+                        {page._menu_item_name}
+                      </Link>
+                    </li>
+                  ))}
+                  {/* {navigation.map((item) => (
                     <a
                       key={item.name}
                       href={item.href}
@@ -176,20 +240,20 @@ export default function Header3(data: any) {
                     >
                       {item.name}
                     </a>
-                  ))}
+                  ))} */}
                 </div>
-                <div className="py-6">
+                {/* <div className="py-6">
                   <a
                     href="#"
                     className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
                   >
                     Log in
                   </a>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </header>
   );
