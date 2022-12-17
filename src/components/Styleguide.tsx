c85667c830a457eba988b47841b3f7c5c0f877c1
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-17 16:15:46 +0100

    wip

--- src/components/Styleguide.tsx
+++ src/components/Styleguide.tsx
@@ -1,7 +1,24 @@
+import Nunjucks from './blocks/bootstrap/Nunjucks';
+import Container from './elements/layout/Container';
+import Section from './elements/layout/Section';
+import Grid from './elements/layout/Grid';
+
+const Components: any = {};
+
 export default function Styleguide(data: any) {
+  console.log(data);
+  const blockData = {
+    block: { _type: 'styleguide', _id: 'styleguide-123456' },
+    theme: data.theme,
+  };
   return (
-    <>
-      <h1>Styleguide</h1>
-    </>
+    <Section type="normal">
+      <Container>
+        <h1 className="pt-4 text-4xl">Styleguide</h1>
+        <div className="mt-6">
+          <Nunjucks {...blockData} />
+        </div>
+      </Container>
+    </Section>
   );
 }
