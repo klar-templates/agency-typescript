b86916138d9e03f9bbb9884b22c1fb676c5c66f8
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-17 22:31:43 +0100

    wip

--- src/components/RenderStyle.tsx
+++ src/components/RenderStyle.tsx
@@ -1,7 +1,7 @@
 export default function RenderStyle({ children, id }: any) {
   let style = `<style>${children}</style>`;
   if (id) {
-    let style = `<style id=${id}>${children}</style>`;
+    style = `<style id=${id}>${children}</style>`;
   }
   return <div dangerouslySetInnerHTML={{ __html: style }} />;
 }
