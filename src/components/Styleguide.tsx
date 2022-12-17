19fa1210b154d65089910b041b287a8bfe7007aa
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-17 21:15:54 +0100

    wip

--- src/components/Styleguide.tsx
+++ src/components/Styleguide.tsx
@@ -15,7 +15,7 @@ export default function Styleguide(data: any) {
     <Section type="normal">
       <Container>
         <h1 className="pt-4 text-4xl">Styleguide</h1>
-        <div className="">
+        <div className="mt-7">
           <Nunjucks {...blockData} />
         </div>
       </Container>
