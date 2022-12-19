8b38740875a3d066c56b962853461efed4f74983
Gabriel Lantz <gabriel.lantz@svt.se>
2022-12-19 18:54:21 +0100

    wip

--- src/components/PageScript.tsx
+++ src/components/PageScript.tsx
@@ -1,6 +1,12 @@
 import RenderScript from './RenderScript';
 
 export default function PageScript() {
+  if (
+    window.releaseReactApp &&
+    window.klarContext.data.pages[0]._id !== window.klarContext.currentPage._id
+  ) {
+    return null;
+  }
   // const pageScript = `
   //   console.log('Från page-script-komponenten');
   // `;
--- src/components/PageStyle.tsx
+++ src/components/PageStyle.tsx
@@ -58,6 +58,13 @@ export default function PageStyle(data: any) {
     }
   });
 
+  if (
+    window.releaseReactApp &&
+    window.klarContext.data.pages[0]._id !== window.klarContext.currentPage._id
+  ) {
+    return null;
+  }
+
   const primary_shades = data.theme.colors.primary_shades.colors;
   const neutral_shades = data.theme.colors.neutral_shades.colors;
   const pageStyle = `
