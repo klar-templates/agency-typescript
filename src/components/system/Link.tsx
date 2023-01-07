7bb10c600aa0799874b4f49abc1f0f05b8a97185
Gabriel Lantz <gabriel.lantz@svt.se>
2023-01-07 18:00:48 +0100

    wip

--- src/components/system/Link.tsx
+++ src/components/system/Link.tsx
@@ -1,35 +1,36 @@
 import { Link as LinkOriginal } from 'react-router-dom';
+import { useRef } from 'react';
 
 export default function Link(data: any) {
+  const linkRef: any = useRef(null);
   console.log('agaaain');
   if (data.to === location.pathname) {
-    console.log(location.pathname);
-    console.log(data.to);
-    const href = data.to;
-    // delete data.to;
-    const newData = {
-      ...data,
-      href: href,
-      onClick: function (e: any) {
-        console.log('nu');
+    // console.log(location.pathname);
+    // console.log(data.to);
+    // const href = data.to;
+    // // delete data.to;
+    // const newData = {
+    //   ...data,
+    //   href: href,
+    //   onClick: function (e: any) {
+    //     e.preventDefault();
+    //   },
+    // };
+    if (linkRef.current) {
+      const el = linkRef.current;
+      el.addEventListener('click', function (e: any) {
         e.preventDefault();
-        e.stopPropagation();
-        return false;
-      },
-      onDoubleClick: function (e: any) {
-        console.log('nu1 double');
-        e.preventDefault();
-        e.stopPropagation();
-        return false;
-      },
-    };
-    return <a {...newData}>{data.children}</a>;
+        console.log('uuu');
+      });
+    }
+    // return <a {...newData}>{data.children}</a>;
     // return (
     //   <a {...newData} style={{ pointerEvents: 'none', cursor: 'pointer' }}>
     //     {data.children}
     //   </a>
     // );
   }
+  const linkData = { ...data, ref: linkRef };
   console.log('nooooo');
-  return <LinkOriginal {...data}>{data.children}</LinkOriginal>;
+  return <LinkOriginal {...linkData}>{data.children}</LinkOriginal>;
 }
