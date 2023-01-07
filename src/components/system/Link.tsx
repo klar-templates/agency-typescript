07836c56039fdba6b1267ce63166d754efebc990
Gabriel Lantz <gabriel.lantz@svt.se>
2023-01-07 22:58:20 +0100

    wip

--- src/components/system/Link.tsx
+++ src/components/system/Link.tsx
@@ -1,8 +1,9 @@
 import { Link as LinkOriginal } from 'react-router-dom';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 export default function Link(data: any) {
   const linkRef: any = useRef(null);
+  const [disable, setDisable] = useState(false);
   useEffect(() => {
     function preventLink(e: any) {
       e.preventDefault();
@@ -11,10 +12,12 @@ export default function Link(data: any) {
     const el = linkRef?.current;
     if (el) {
       // if (data.to === location.pathname) {
-      if (linkRef.current) {
-        el.addEventListener('click', preventLink);
+      if (disable) {
+        console.log('disable');
+        if (linkRef.current) {
+          el.addEventListener('click', preventLink);
+        }
       }
-      // }
     }
     return () => {
       if (el) {
@@ -28,8 +31,17 @@ export default function Link(data: any) {
         }
       }
     };
-  }, []);
+  }, [disable]);
   const linkData = { ...data, ref: linkRef };
   // console.log('Link loaded!');
-  return <LinkOriginal {...linkData}>{data.children}</LinkOriginal>;
+  return (
+    <LinkOriginal
+      {...linkData}
+      onClick={function () {
+        setDisable(true);
+      }}
+    >
+      {data.children}
+    </LinkOriginal>
+  );
 }
