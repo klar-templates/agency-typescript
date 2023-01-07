411405e4634b6c70c10143efb7ee3bb848970082
Gabriel Lantz <gabriel.lantz@svt.se>
2023-01-07 18:12:46 +0100

    wip

--- src/components/system/Link.tsx
+++ src/components/system/Link.tsx
@@ -1,17 +1,34 @@
 import { Link as LinkOriginal } from 'react-router-dom';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function Link(data: any) {
   const linkRef: any = useRef(null);
-  if (data.to === location.pathname) {
-    if (linkRef.current) {
-      const el = linkRef.current;
-      el.addEventListener('click', function (e: any) {
-        e.preventDefault();
-        console.log('uuu');
-      });
+  useEffect(() => {
+    function preventLink(e: any) {
+      e.preventDefault();
+      console.log('uuu');
     }
-  }
+    const el = linkRef?.current;
+    if (el) {
+      if (data.to === location.pathname) {
+        if (linkRef.current) {
+          el.addEventListener('click', preventLink);
+        }
+      }
+    }
+    return () => {
+      if (el) {
+        try {
+          // console.log(containerRef.current);
+          // containerRef?.current.removeChild(script);
+          el.removeEventListener('click', preventLink);
+          console.log('tog bort event');
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    };
+  }, []);
   const linkData = { ...data, ref: linkRef };
   console.log('Link loaded!');
   return <LinkOriginal {...linkData}>{data.children}</LinkOriginal>;
