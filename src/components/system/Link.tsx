5157fdb5fc928032f8f89b4a0a7532128f5ea93c
Gabriel Lantz <gabriel.lantz@svt.se>
2023-01-07 14:10:18 +0100

    wip

--- src/components/system/Link.tsx
+++ src/components/system/Link.tsx
@@ -1,9 +1,10 @@
 import { Link as LinkOriginal } from 'react-router-dom';
 
 export default function Link(data: any) {
-  console.log(location.pathname);
-  console.log(data.to);
-  if (data.href === location.pathname) {
+  if (data.to === location.pathname) {
+    console.log(location.pathname);
+    console.log(data.to);
+    data.href == data.to;
     return <a {...data}>{data.children}</a>;
   }
   return <LinkOriginal {...data}>{data.children}</LinkOriginal>;
