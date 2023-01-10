3eed00a3359c5afc7de2fef04328293bdc7ff61d
Gabriel Lantz <gabriel.lantz@svt.se>
2023-01-10 13:27:51 +0100

    wip

--- dist/assets/index.b2c27f3a.js
+++ dist/assets/index.b2c27f3a.js
@@ -399,7 +399,7 @@ else {`),this._emit("cb()")),this._emitLine("}")},k.compileIfAsync=function(g,m)
           class="bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF7_51.02%,#4D78EF_79.09%)] bg-clip-text text-5xl leading-[1.2] tracking-tighter text-transparent sm:text-center sm:text-[4rem] sm:leading-[4.75rem] lg:text-left"
           data-field-string="title"
         >
-          {{block.title}} \xE5\xE4\xF6 \xE5\xE4\xF6 \xE5\xE4\xF6
+          {{block.title}}
         </h1>
         <p
           class="mt-6 max-w-3xl text-2xl leading-[2.5rem] tracking-tight sm:text-center lg:text-left"
