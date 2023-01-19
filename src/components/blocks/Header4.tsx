8e85eae3ae01d75b3cd3dbab49303f78ba12a4f6
Gabriel Lantz <gabriel.lantz@svt.se>
2023-01-19 22:38:10 +0100

    wip

--- src/components/blocks/Header4.tsx
+++ src/components/blocks/Header4.tsx
@@ -121,14 +121,18 @@ export default function Header4(data: any) {
             {navigation.map((item: any, index: any) => (
               <li key={index}>
                 <a
-                  className="page-scroll block font-semibold text-neutral-900 hover:text-primary dark:text-neutral-200 dark:hover:text-primary list-none whitespace-nowrap"
                   href={item.link}
                   target={item.open_in_new_page ? '_blank' : '_self'}
-                  data-field-string={`navigation[${index}].title`}
-                  data-field-string-format-uri={`navigation[${index}].link`}
-                  data-placement="bottom"
+                  className="page-scroll block"
                 >
-                  {item.title}
+                  <div
+                    className="font-semibold text-neutral-900 hover:text-primary dark:text-neutral-200 dark:hover:text-primary list-none whitespace-nowrap"
+                    data-field-string={`navigation[${index}].title`}
+                    data-field-string-format-uri={`navigation[${index}].link`}
+                    data-placement="bottom"
+                  >
+                    {item.title}
+                  </div>
                 </a>
               </li>
             ))}
